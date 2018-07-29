7f7e91f23fd3d767256d1d0ea8db4d9c6d1d5766
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-07-29 23:02:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -99,24 +99,35 @@ mentionned.send(` :credit_card: | Transfer Receipt \`\`\`You have received ${arg
         })
         })
         
-		
-client.on("ready", async  => {
-setInterval(function(){
-client.channels.find('id', '469433733758451740').setName("W");
-client.channels.find('id', '469433733758451740').setName("We");
-client.channels.find('id', '469433733758451740').setName("Wel");
-client.channels.find('id', '469433733758451740').setName("Welc");
-client.channels.find('id', '469433733758451740').setName("Welco");
-client.channels.find('id', '469433733758451740').setName("Welcom");
-client.channels.find('id', '469433733758451740').setName("Welcome");
-client.channels.find('id', '469433733758451740').setName("Welcome T");
-client.channels.find('id', '469433733758451740').setName("Welcome To");
-client.channels.find('id', '469433733758451740').setName("Welcome To C");
-client.channels.find('id', '469433733758451740').setName("Welcome To Co");
-client.channels.find('id', '469433733758451740').setName("Welcome To Cod");
-client.channels.find('id', '469433733758451740').setName("Welcome To Code");
-client.channels.find('id', '469433733758451740').setName("Welcome To Codes");
-  }, 3000);
+  client.on('message', message => {
+      if(message.content.startsWith ("!marry")) {
+      if(!message.channel.guild) return message.reply('** This command only for servers **')
+      var proposed = message.mentions.members.first()
+     
+      if(!message.mentions.members.first()) return message.reply(' 😏 **لازم تطلب ايد وحدة**').catch(console.error);
+      if(message.mentions.users.size > 1) return message.reply(' 😳 **ولد ما يصحلك الا حرمة وحدة كل مرة**').catch(console.error);
+       if(proposed === message.author) return message.reply(`**خنثى ؟ **`);
+        if(proposed === client.user) return message.reply(`** تبي تتزوجني؟ **`);
+              message.channel.send(`**${proposed} 
+ بدك تقبلي عرض الزواج من ${message.author} 
+ العرض لمدة 15 ثانية  
+ اكتبي موافقة او لا**`)
+
+const filter = m => m.content.startsWith("موافقة");
+message.channel.awaitMessages(filter, { max: 1, time: 15000, errors: ['time'] })
+.then(collected =>{ 
+    message.channel.send(` **${message.author} و ${proposed} الف الف مبروك الله , يرزقكم الذرية الصالحة** `);
+})
+
+   const filte = m => m.content.startsWith("لا");
+message.channel.awaitMessages(filte, { max: 1, time: 15000, errors: ['time'] })
+.then(collected =>{ 
+   message.channel.send(`  **${message.author} تم رفض عرضك** `);
+})
+        
+  }
+});
+  
 });
 
 
