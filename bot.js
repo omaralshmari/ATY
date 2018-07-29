e2f07378b054297414c50d6294f505c4afde0b93
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-07-29 20:38:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 	const Discord = require('discord.js');
 	const client = new Discord.Client()
+	 const prefix = "!";
 	const fs = require('fs');
 	const moment = require('moment');
 let profile = JSON.parse(fs.readFileSync("./profile.json", "utf8"))
