14c1dc339af9cd967cc935b7d8fc9abac52a371d
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-07-30 01:40:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -267,5 +267,22 @@ message.channel.send(`**:white_check_mark: ${user.tag} banned from the server ! 
 }
 });
   
+  client.on('ready', function(){
+    var ms = 1000 ;
+    var setGame = [`!i `,` !in`,`!inv`,`!invi`,`!invit`,`!invite`];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/idk`);
+    }, ms);1000
+
+});
   
 client.login(process.env.BOT_TOKEN); 
