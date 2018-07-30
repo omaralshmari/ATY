cc82e86c7b683874c4d7216cda8b263ae100e87c
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-07-31 02:39:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -138,32 +138,6 @@ rebel.guild.owner.send(join)
 });
   }
 });
-  client.on('message', message => {
-    if (message.author.bot) return;
-     if (message.content === prefix + "invite") {
-
-
- message.author.sendMessage(`
- 
- __~~ATY~~__
-╔═══╦════╦╗──╔╗────╔══╗╔═══╦════╗
-║╔═╗║╔╗╔╗║╚╗╔╝║────║╔╗║║╔═╗║╔╗╔╗║
-║║─║╠╝║║╚╩╗╚╝╔╝────║╚╝╚╣║─║╠╝║║╚╝
-║╚═╝║─║║──╚╗╔╝─╔══╗║╔═╗║║─║║─║║
-║╔═╗║─║║───║║──╚══╝║╚═╝║╚═╝║─║║
-╚╝─╚╝─╚╝───╚╝──────╚═══╩═══╝─╚╝
-♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ ♕ 
- __created By__: ATY
-
-Server Support : https://discord.gg/cEtEXru
-
-bot link : https://goo.gl/AmyZY7
-`);
-
-message.channel.send('**تم الارسال في الخاص**');
-
-    }
-});
 
 client.on('message', async message => {
   if(message.content.startsWith(prefix + "tc")) {
@@ -232,7 +206,7 @@ message.channel.send(`This avatar For ${user} link : ${user.avatarURL}`);
   
   client.on('ready', function(){
     var ms = 1000 ;
-    var setGame = [`!help `,` !invite`];
+    var setGame = [`ATY`,`!help`,ََ`A`,`AT`,`ATY`];
     var i = -1;
     var j = 0;
     setInterval(function (){
@@ -744,7 +718,7 @@ client.on('message', message => {
  })
 client.on('message', message => {
     if (message.author.bot) return;
-     if (message.content === prefix + "help-ar") {
+     if (message.content === prefix + "help") {
 		 message.channel.send('**تم ارسالك في الخاص**');
             
 	
@@ -784,11 +758,11 @@ client.on('message', message => {
 
 ❖ !date ➾ التاريخ
 
-❖ *ping ➾ عرض سرعه اتصال البوت
+❖ !ping ➾ عرض سرعه اتصال البوت
 
-❖ *bot ➾ معلومات البوت
+❖ !bot ➾ معلومات البوت
 
-❖ *server ➾ معلومات السيرفر
+❖ !server ➾ معلومات السيرفر
 
 `);
 
@@ -850,6 +824,78 @@ client.on("message", message => {
         }
     });
 
+
+
+let points = JSON.parse(fs.readFileSync('./typing/typePTS.json', 'utf8')); // يقوم بقراءه ملف النقاط , والمسار حق النقاط
+
+client.on('message', message => {
+if (!points[message.author.id]) points[message.author.id] = { // يقوم الكود تلقائياً في حال لم يجد نقاط العضو بإنشاء نقاط له ويتم إرسالها الملف المخصص
+	points: 0,
+  };
+if (message.content.startsWith(prefix + 'سرعة')) { // .سرعة
+	if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
+
+const type = require('./typing/type.json'); // في هذا السطر يقوم الكود بقراءة ملف الأسئلة
+const item = type[Math.floor(Math.random() * type.length)]; // الأرراي المخصص للأسئلة
+const filter = response => { // في هذا السطر يقوم بصنع فلتر للأجوبة
+    return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
+};
+message.channel.send('**لديك 15 ثانية لكتابة الكلمة**').then(msg => {
+	let embed = new Discord.RichEmbed()
+	.setColor('#000000')
+	.setFooter("سرعة كتابة | لرؤية مجموع نقاطك اكتب $نقاطي |", 'https://c.top4top.net/p_814rjkod1.png')
+	.setDescription(`**قم بكتابة : ${item.type}**`) // ${item.type} = السؤال
+	
+	msg.channel.sendEmbed(embed).then(() => {
+        message.channel.awaitMessages(filter, { maxMatches: 1, time: 15000, errors: ['time'] })
+        .then((collected) => {
+		message.channel.send(`${collected.first().author} ✅ **لقد قمت بكتابة الكلمة بالوقت المناسب**`);
+		console.log(`[Typing] ${collected.first().author} typed the word.`);
+            let won = collected.first().author; // في هذا السطر يقوم الكود بسحب الأي دي الذي قام بالأجابة اولاً
+            points[won.id].points++;
+          })
+          .catch(collected => { // في حال لم يقم أحد بالإجابة
+            message.channel.send(`:x: **لم يقم أحد بكتابة الجملة بالوقت المناسب**`);
+			console.log(`[Typing] Error: No one type the word.`);
+          })
+		})
+	})
+}
+});
+client.on('message', message => {
+if (message.content.startsWith(prefix + 'نقاطي')) {
+	if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
+	let userData = points[message.author.id];
+	let embed = new Discord.RichEmbed()
+    .setAuthor(`${message.author.tag}`, message.author.avatarURL)
+	.setColor('#000000')
+	.setFooter("بوت سرعة الكتابة", '')
+	.setDescription(`نقاطك: \`${userData.points}\``)
+	message.channel.sendEmbed(embed)
+  }
+  fs.writeFile("./typePTS.json", JSON.stringify(points), (err) => {
+    if (err) console.error(err)
+  })
+});
+client.on('guildCreate', guild => {
+	console.log(`Added to a server by: ${guild.owner.user.username} || Server name: ${guild.name} || Users: ${guild.memberCount}`); // ايفنت يقوم بإرسال إلى الكونسل بأنه قد قامت احد السيرفر بدعوة البوت
+});
+client.on('message', message => {
+if (message.content.startsWith(prefix + 'help')) {
+	if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(300));
+	let embed = new Discord.RichEmbed()
+    .setAuthor(`${message.author.tag}`, message.author.avatarURL)
+	.setColor('RANDOM')
+	.addField(".سرعة","**لبدأ لعبة سرعة الكتابة**")
+	.addField(".نقاطي","**لعرض النقاط الخاصة بك**")
+.addField("**--------------------------------**","** **")
+.addField("**بعض لاالعاب قادمه وشكرا**","** **")
+ .addField("** BY :wrench: **","**™ » <@431150885549113344>**")
+	.setFooter("Games-Bot", 'https://cdn3.iconfinder.com/data/icons/forall/110/game-128.png')
+	message.channel.sendEmbed(embed).then(m => m.delete(15000));
+
+}
+});
 
 
   
