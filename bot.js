d14a50b88b44dc8548c43a6669ff6af3de78766a
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-07-30 15:45:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -337,20 +337,6 @@ client.on('message', message => {
     }
 });
 
-var prefix = "!"
-client.on('message', message => {
 
-  if (message.content.startsWith( prefix + "sug")) {
-  if (!message.channel.guild) return;
-  let args = message.content.split(" ").slice(1).join(' ');
-  client.channels.get("#470995791796764702").send(
-      "\n" + "**" + "● السيرفر :" + "**" +
-      "\n" + "**" + "» " + message.guild.name + "**" +
-      "\n" + "**" + " ● المرسل : " + "**" +
-      "\n" + "**" + "» " + message.author.tag + "**" +
-      "\n" + "**" + " ● الرسالة : " + "**" +
-      "\n" + "**" + args + "**")
-  }
-  });
      
 client.login(process.env.BOT_TOKEN); 
