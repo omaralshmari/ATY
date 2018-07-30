0cb72f9896454bbb9cee74e54b72759561e70e90
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-07-30 23:09:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -383,27 +383,6 @@ client.on("guildCreate", guild => {
   console.log(` شخص ما اضاف بوت  في سيرفر اسمه ! ${guild.name} اونر سيرفر هو ${guild.owner.user.username}!`)
 });
 
-client.on('guildMemberAdd', member => {
-    let channel = member.guild.channels.find('name', 'chat');
-    let memberavatar = member.user.avatarURL
-      if (!channel) return; 
-    let embed = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .setThumbnail(memberavatar)
-        .addField(':running_shirt_with_sash: | name :  ',`${member}`)
-        .addField(':loudspeaker: | نورت السيرفر ي قلبي' , `Welcome to the server, ${member}`)
-        .addField(':id: | user :', "**[" + `${member.id}` + "]**" )
-                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
-               
-                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
-                      
-                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
-                                       
-     .setFooter("**SERVER NAME **")
-        .setTimestamp()
-    
-      channel.sendEmbed(embed);
-    });
 
 client.on('guildMemberAdd', member=> {
     member.addRole(member.guild.roles.find("name","Active"));
@@ -419,5 +398,22 @@ var channel =member.guild.channels.find('name', 'chat')
 if (!channel) return;
 channel.send({embed : embed});
 });
+client.on("guildMemberAdd", (member) => {
+    const guild = member.guild;
+    if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
+    newUsers[guild.id].set(member.id, member.user);
+  
+    if (newUsers[guild.id].size > 2) {
+      const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
+      guild.channels.get(guild.id).send("Welcome our new users!\n" + userlist);
+      newUsers[guild.id].clear();
+    }
+  });
+  
+  client.on("guildMemberRemove", (member) => {
+    const guild = member.guild;
+    if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
+  });
+  
 
 client.login(process.env.BOT_TOKEN); 
