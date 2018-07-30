d071ac7d98bd528b95fe02152d61c150c41ad573
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-07-30 23:05:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -408,6 +408,16 @@ client.on('guildMemberAdd', member => {
 client.on('guildMemberAdd', member=> {
     member.addRole(member.guild.roles.find("name","Active"));
     });
-
+client.on('guildMemberAdd', member => {
+    var embed = new Discord.RichEmbed()
+    .setThumbnail(member.user.avatarURL)
+  .addField("***شكرا الانضمامك الينا***" ,member.user.username )
+    .setDescription('***بكل حب واحترام وشوق نستقبلك ونتمنى لك قضآء أجمل اللحظات ولآوقات معنا***')
+    .setColor('RANDOM')
+    .setImage('http://www.imgion.com/images/01/Welcome-buddy.jpg')
+var channel =member.guild.channels.find('name', 'chat')
+if (!channel) return;
+channel.send({embed : embed});
+});
 
 client.login(process.env.BOT_TOKEN); 
