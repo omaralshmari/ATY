6a10b836d81f7fcb81aa3943e48b59d56dd92152
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-07-30 16:00:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -362,5 +362,23 @@ client.on('message', message => {
       message.author.sendEmbed(Embed11)
     }
 });
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '!'
+client.on('ready', () => {
+  console.log('╔[════════════════════════════════════]╗');
+  console.log('')
+  console.log('            ╔[════════════]╗')
+  console.log('              Bot Is Online')
+  console.log('            ╚[════════════]╝')
+  console.log('')
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log('')
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log('')
+  console.log('╚[════════════════════════════════════]╝')
+});
      
 client.login(process.env.BOT_TOKEN); 
