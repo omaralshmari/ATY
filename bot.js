76def2acf8a9ebe4dfd0ad0a363a4fd98e5b16c7
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-07-31 16:49:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -811,41 +811,4 @@ client.on("message", message => {
             return;
         }
     });
-const Eris = require("eris");
-var codes = new Eris("Token");
-var codes_id = "473795651155787796";
-                    var i = "0";
-                    var x = "0";
-codes.on("voiceChannelJoin", (msg) => {
-    x++;
-    codes.editChannel(codes_id, { name : "Voice ⇏「" + x + "」"});
-});
-codes.on("voiceChannelLeave", (msg) => {
-    x--;
-    codes.editChannel(codes_id, { name : "Voice ⇏「" + x + "」"});
-});
-
-codes.on("messageCreate", (msg) => {
-    if(msg.author.id !== "Owner id") return codes.createMessage('__**This Command is only for the bot Owner**__');
-    if(msg.content === "$voice") {
-        let users = msg.channel.guild.members.map(m => m.user.id);
-        let messages = [];
-        messages.push(users);
-        setTimeout(function(){
-        while (i <= messages[0].length - 1) {
-            check = msg.channel.guild.members.get(messages[0][i]);
-        if(!check.voiceState.channelID){
-                i++;
-        }else{
-                x++;
-                i++;
-        }
-}
-    console.log(x);
-    codes.createMessage(msg.channel.id, "Voice Online Members Now Are: **"+x+"** Members!");
-    codes.editChannel(codes_id, { name : "Voice ⇏「"+x+"」"});
-    messages = [];
-}, 1);
-    }
-});
 
