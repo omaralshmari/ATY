dbe53f34115d062d6e5785b707aadb26ca8d940a
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-08-04 18:53:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -910,7 +910,8 @@ client.on("guildMemberAdd", (member) => {
               })
             }
           })
-  let profile = JSON.parse(fs.readFileSync("./profile.json", "utf8"))
+
+  let profile = JSON.parse('{}'); 
 client.on("message", message => {
  
   if (message.author.bot) return;
