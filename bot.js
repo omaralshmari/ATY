75b636493f1487e43b855b9554b7647b0022096a
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-08-04 21:40:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1082,6 +1082,44 @@ message.channel.send('**لديك 15 ثانية لكتابة الكلمة**').the
 	})
 }
 });
+
+
+client.on('message', message => {
+if (!points[message.author.id]) points[message.author.id] = { // يقوم الكود تلقائياً في حال لم يجد نقاط العضو بإنشاء نقاط له ويتم إرسالها الملف المخصص
+	points: 0,
+  };
+if (message.content.startsWith(prefix + 'لغز')) { // .لغز
+	if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
+
+
+const type = require('./lgz/nope.json'); // في هذا السطر يقوم الكود بقراءة ملف الأسئلة
+const item = type[Math.floor(Math.random() * type.length)]; // الأرراي المخصص للأسئلة
+const filter = response => { // في هذا السطر يقوم بصنع فلتر للأجوبة
+    return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
+};
+message.channel.send('**لديك 15 ثانية لكتابة الكلمة**').then(msg => {
+	let embed = new Discord.RichEmbed()
+	.setColor('#000000')
+	.setFooter(" ة مجموع نقاطك  |بوت لغز", 'https://c.top4top.net/p_814rjkod1.png')
+	.setDescription(`**قم بكتابة : ${item.type}**`) // ${item.type} = السؤال
+	///by ™¦༺♚ƙἶղց|MaS♚༺¦™#9506
+
+	msg.channel.sendEmbed(embed).then(() => {
+        message.channel.awaitMessages(filter, { maxMatches: 1, time: 15000, errors: ['time'] })
+        .then((collected) => {
+		message.channel.send(`${collected.first().author} ✅ **لقد قمت بكتابة الكلمة بالوقت المناسب**`);
+		console.log(`[Typing] ${collected.first().author} typed the word.`);
+            let won = collected.first().author; // في هذا السطر يقوم الكود بسحب الأي دي الذي قام بالأجابة اولاً
+            points[won.id].points++;
+          })
+          .catch(collected => { // في حال لم يقم أحد بالإجابة
+            message.channel.send(`:x: **لم يقم أحد بكتابة الجملة بالوقت المناسب**`);
+			console.log(`[Typing] Error: No one type the word.`);
+          })
+		})
+	})
+}
+});
 client.on('message', message => {
 if (message.content.startsWith(prefix + 'نقاطي')) {
 	if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
@@ -1093,8 +1131,7 @@ if (message.content.startsWith(prefix + 'نقاطي')) {
 	.setDescription(`نقاطك: \`${userData.points}\``)
 	message.channel.sendEmbed(embed)
   }
-  fs.writeFile("./lgz/typePTS.json", JSON.stringify(points), (err) => {
-    if (err) console.error(err)
+const type = require('./lgz/typePTS.json');
   })
 });
 client.on('guildCreate', guild => {
@@ -1113,7 +1150,8 @@ if (message.content.startsWith(prefix + 'help')) {
 	message.channel.sendEmbed(embed).then(m => m.delete(15000));
 
 }
-})
+});
+
 
 client.login(process.env.BOT_TOKEN); 
 
