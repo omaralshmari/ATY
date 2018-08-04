bce54b92bec9a06335d95b88e31d0eb8815ade96
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-08-04 16:21:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -895,6 +895,22 @@ client.on("guildMemberAdd", (member) => {
     });
 
 });
+  client.on('message', async message => {
+  if(message.content.startsWith(prefix + "sug")) {
+  await  message.channel.send(`اكتب اقتراحك الان`)
+    let filter = m => m.author.id === message.author.id
+      var text = '';
+        let sugsa = message.channel.awaitMessages(filter, { max: 1, time: 60000})
+          .then(co => {
+            text = co.first().content
+
+              message.channel.send(`تم حفظ اقتراحك الرجاء انتضار الرد من قبل الاداره`)
+                client.channels.get("475292101442142208").send(`${message.author.username}'s sug => ${text}`)
+
+              })
+            }
+          })
+  
 
 
 client.login(process.env.BOT_TOKEN); 
