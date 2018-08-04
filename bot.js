652875d62ce9da1a3bcb9ede9aacc18559ae922d
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-08-04 20:20:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1000,8 +1000,9 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     console.log(`in ${client.guilds.size} servers `)
     console.log(`[Users] ${client.users.size}`)
-});
-let points = JSON.parse('./lgz/typePTS.json', 'utf8'); // يقوم بقراءه ملف النقاط , والمسار حق النقاط
+});let profile = JSON.parse('{}'); 
+
+let points = JSON.parse({./lgz/typePTS.json}, 'utf8'); // يقوم بقراءه ملف النقاط , والمسار حق النقاط
 ; 
 ///by ™¦༺♚ƙἶղց|MaS♚༺¦™#9506
 
